82e0564e18e4204bd5471e559c166422cea25a01
Shaked Caspi <caspi5032@gmail.com>
2023-03-26 14:42:30 +0300

    changes in question1

--- hw1/programming_part/question1/mnist_utils.py
+++ hw1/programming_part/question1/mnist_utils.py
@@ -13,10 +13,11 @@ def loss_plot(values, model_name, optimizer, learning_rate, batch_size, num_epoc
     plt.plot(values, color="darkred")
     plt.legend(['training loss'], loc='upper right')
     plt.xlim((0, num_epochs))
-    plt.xticks(np.arange(0, num_epochs + 2, step=10))
+    plt.xticks(np.arange(0, num_epochs+1, 5, dtype=np.int32))
     plt.title(
-        f"{model_name} \n\nOptimizer: {optimizer}      Learning Rate = {learning_rate}\nBatch Size = {batch_size}     Epoch Number = {num_epochs}", fontdict=font)
+        f"{model_name} \nOptimizer: {optimizer}      Learning Rate = {learning_rate}\nBatch Size = {batch_size}   Epoch Number = {num_epochs}", fontdict=font)
     plt.xlabel('Epochs', fontdict=font)
     plt.ylabel('Cross Entropy Loss', fontdict=font)
+    plt.tight_layout()
     plt.savefig(f"{file_name}.png")
     plt.show()
